77fa6d386984075d476ab874317bd2122d0bc9f7
Jennie <jen.nie.333.chen@gmail.com>
2024-03-05 17:13:33 -0500

    routes, navigation, and tailwind css

---
+++ my-app/app/components/AddToCard.tsx
@@ -0,0 +1,14 @@
+'use client'; // tells next.js compiler to include this component in our js bundle
+import React from 'react';
+
+const AddToCard = () => {
+  return (
+    <div>
+      <button className='btn btn-primary' onClick={() => console.log('Click')}>
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCard;
--- my-app/app/users/page.tsx
+++ my-app/app/users/page.tsx
@@ -1,7 +1,31 @@
+import { LargeNumberLike } from 'crypto';
 import React from 'react';
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+    // when using fetch, it will take from cache data
+    // cache: 'no-store', // need fresh data, disable caching
+    // next: { revalidate: 10 }, // fresh data for a certain period of time
+  });
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>{' '}
+      {/* Dynamic Rendering only on build time, when it gets to a website it switches to Static Rendering */}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
